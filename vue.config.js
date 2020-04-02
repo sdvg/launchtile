ffd9866968813282f361363e2ed77bd4dc278010
Stefan Dietz <sdvg@users.noreply.github.com>
2020-04-02 17:36:11 +0200

    Fix dev version

--- vue.config.js
+++ vue.config.js
@@ -13,10 +13,12 @@ module.exports = {
   },
   pluginOptions: {
     browserExtension: {
-      componentOptions: {
-        background: {
-          entry: `src/background.js`,
-        },
+      manifestTransformer: manifest => {
+        if (process.env.NODE_ENV === `development`) {
+          manifest.content_security_policy = `script-src 'self' 'unsafe-eval'; object-src 'self'; img-src chrome://favicon;`
+        }
+
+        return manifest
       },
     },
   },
