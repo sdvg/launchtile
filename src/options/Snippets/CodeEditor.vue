76156e41af25eb49a00a0c7097cb566ed55cd168
Stefan Dietz <sdvg@users.noreply.github.com>
2021-01-24 17:42:25 +0100

    Add editor search plugins (#5)

--- src/options/Snippets/CodeEditor.vue
+++ src/options/Snippets/CodeEditor.vue
@@ -6,6 +6,11 @@
   import 'codemirror/addon/hint/javascript-hint'
   import 'codemirror/addon/hint/show-hint'
   import 'codemirror/addon/hint/show-hint.css'
+  import 'codemirror/addon/selection/mark-selection'
+  import 'codemirror/addon/dialog/dialog'
+  import 'codemirror/addon/dialog/dialog.css'
+  import 'codemirror/addon/search/searchcursor'
+  import 'codemirror/addon/search/search'
   import 'codemirror/theme/3024-day.css'
   import 'codemirror/theme/moxer.css'
 
@@ -38,6 +43,7 @@
         extraKeys: { 'Ctrl-Space': `autocomplete` },
         placeholder: `Code goes here ...`,
         tabSize: 2,
+        styleSelectedText: true,
       })
 
       this.codeMirror.on(`change`, this.onChange)
@@ -77,5 +83,16 @@
 
   :global(.CodeMirror) {
     flex-grow: 1;
+  }
+
+  :global(.cm-searching.CodeMirror-selectedtext) {
+    background-color: #ffa;
+    color: #4e4e4e !important;
+  }
+
+  @media(prefers-color-scheme: light) {
+    :global(.cm-searching.CodeMirror-selectedtext) {
+      color: #4e4e4e !important;
+    }
   }
 </style>
